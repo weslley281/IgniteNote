f2f17fec68011cee958a4de836e831a76b125ebd
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-06-15 20:06:28 -0400

    a

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,5 @@
+import '../../styles/global.css';
+
 export function App() {
-  return (
-    <div style={{ fontFamily: 'Inter' }}>
-      <h1>Olá Mundo</h1>
-      <h2>Weslley é foda</h2>
-    </div>
-  );
+  return <h1 className="text-violet-400 font-bold text-4xl">Hello World</h1>;
 }
