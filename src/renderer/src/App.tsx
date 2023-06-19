86c03549ad03393a3753a88f5cd5f2c00723026c
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-06-19 18:07:09 -0300

    a

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,8 @@
 import '../../styles/global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
-    <div className="h-screen w-screen bg-rotion-900 text-rotion-100">aaa</div>
+    <div className="h-screen w-screen bg-rotion-900 text-rotion-100"><Sidebar /><div></div></div>
   );
 }
