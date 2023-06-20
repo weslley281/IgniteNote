65c4a431a252df973815a498108ff2c5ebbfb46d
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-06-20 10:09:17 -0400

    a

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,17 @@
 import '../../styles/global.css';
+import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
-    <div className="h-screen w-screen bg-rotion-900 text-rotion-100"><Sidebar /><div></div></div>
+    <div className="h-screen w-screen bg-rotion-900 text-rotion-100 flex">
+      <Sidebar />
+      <div className="flex-1 flex flex-col max-h-screen">
+        <Header />
+        <main className="flex-1 flex intens-center justify-center text-rotion-400">
+          Selecione ou crie um documento
+        </main>
+      </div>
+    </div>
   );
 }
